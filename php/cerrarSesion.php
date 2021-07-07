17e012a5af0b17e2be5ae0941962c97cd673e8a7
felipeangelcerdacontreras <felipeangelcerdacontreras@gmail.com>
2021-07-06 23:12:50 -0500

    php

---
+++ php/cerrarSesion.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+
+	unset($_SESSION['usuario']); 
+	unset($_SESSION['logged']);
+	unset($_SESSION['id']);
+	unset($_SESSION['tipo']);
+	unset($_SESSION['nombre']);
+  
+	session_destroy();
+
+	echo "localhost/punto";
+?>
---
+++ php/login.php
@@ -0,0 +1,38 @@
+﻿<?php
+	session_start();
+	include "../db/conexion_1.php";
+
+	$ret = array();
+	$ret['error'] = "Usuario y/o Contraseña Incorrecta.";
+	$ret['success'] = false;
+
+	$user = $_POST['user'];
+	$pass = md5($_POST['pass']);
+
+	$result = mysqli_query($con, "Select usuarios.*, nombre from usuarios inner join datos_usuarios on datos_usuarios.id_user = usuarios.id_user where user = '$user' and pass = '$pass'");
+	
+	if($usuario = mysqli_fetch_array($result)) {
+		$_SESSION['usuario'] = $usuario['user'];
+		$_SESSION['logged'] = "OK";
+		$_SESSION['id'] = $usuario['id'];
+		$_SESSION['tipo'] = $usuario['tipo'];
+		$_SESSION['nombre'] = $usuario['nombre'];
+		$_SESSION['sucursal'] = $usuario['sucursal'];
+		$_SESSION['id_user'] = $usuario['id_user'];
+
+
+	if($_SESSION['logged'] == "OK" && $_SESSION['tipo'] == "1" ) {
+			$ret['success'] = true;
+			$ret['page'] = "Punto_Venta/admin.php";
+		}
+	else if($_SESSION['logged'] == "OK" && $_SESSION['tipo'] == "2" ) {
+			$ret['success'] = true;
+			$ret['page'] = "Punto_Venta/modulos.php";
+		}
+	} else {
+		$ret['success'] = false;
+		$ret['error'] = "Usuario y/o Contraseña Incorrecta.";
+		die(json_encode($ret));
+	}
+	echo json_encode($ret);
+?>
